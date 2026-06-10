1fd6abe6e8f18d192ee84a1938dd0c7eeb1bd468
vansh-gautam-bit <vansh.pku@gmail.com>
2026-06-11 01:51:56 +0530

    updated the owners model

--- models/owner.py
+++ models/owner.py
@@ -6,12 +6,12 @@ from sqlalchemy.orm import relationship
 class Owner(Base):
     __tablename__="owners"
 
-    owner_id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True)
 
-    owner_name  = Column(String, nullable=False)
+    name  = Column(String, nullable=False)
     phone  = Column(String, nullable=False)
-    email = Column(Integer, nullable=False)
+    email = Column(String, nullable=False)
 
-    created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
+    created_at = Column(DateTime, unique=True, default=datetime.utcnow, nullable=False)
 
-    pet = relationship( "pet", back_populates="owner")
+    pets = relationship( "Pet", back_populates="owner")
