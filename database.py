d78beedab34acae8141a35dbb1df868ccaf416dc
vansh-gautam-bit <vansh.pku@gmail.com>
2026-06-10 17:17:55 +0530

    updated database

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///.vet.db"
+DATABASE_URL = "sqlite:///./vet.db"
 
 engine = create_engine(
     DATABASE_URL,
