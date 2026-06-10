6d17804d58b61366d649999a4d979fbaf1ee49d5
vansh-gautam-bit <vansh.pku@gmail.com>
2026-06-10 18:41:05 +0530

    Veterinary Patient Management API is running

---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from database import Base , engine
+from models.pet import Pet
+from models.visit import Visit
+from routers.pets import router as pet_router
+from routers.visits import router as visit_router
+
+Base.metadata.create_all(bind=engine)
+
+app=FastAPI(
+    title = "Veterinary Patient Management API"
+)
+
+app.include_router(pet_router)
+app.include_router(visit_router)
+
+@app.get("/")
+def root():
+    return {
+        "message": "Veterinary Patient Management API is running"
+    }
