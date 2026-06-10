11c1d86e89aeafc3e94bb17f3846ba977006e8da
vansh-gautam-bit <vansh.pku@gmail.com>
2026-06-10 17:42:39 +0530

    Created PetUpdate in met schema

--- schemas/pet.py
+++ schemas/pet.py
@@ -1,6 +1,16 @@
 from pydantic import BaseModel
 from datetime import datetime
 
+class petupdate(BaseModel):
+    name: str
+    species: str
+    breed: str
+    age:int
+    owner_name: str
+    owner_phone: str
+
+
+
 class PetBase(BaseModel):
     name:str
     species:str
