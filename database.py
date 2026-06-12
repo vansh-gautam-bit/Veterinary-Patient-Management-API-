05c3ca294dfc2cae3c42cbdb108e24697f6f05df
vansh-gautam-bit <vansh.pku@gmail.com>
2026-06-12 20:38:04 +0530

    made the database link private

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from config import settings
 
-DATABASE_URL = "sqlite:///./vet.db"
+DATABASE_URL = settings.DATABASE_URL
 
 engine = create_engine(
     DATABASE_URL,
