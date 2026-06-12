92a77b2851a0836b20c59941187fd4c9992dd995
vansh-gautam-bit <vansh.pku@gmail.com>
2026-06-12 16:39:46 +0530

    changed the outlook

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app.include_router(pet_router)
 app.include_router(visit_router)
 app.include_router(owner_router)
 
-@app.get("/")
+@app.get("/",tags = ["Json Page"])
 def root():
     return {
         "message": "Veterinary Patient Management API is running"
