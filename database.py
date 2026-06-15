a03ea9fdaf00e681e698e635dcea711d79e64d5f
vansh-gautam-bit <vansh.pku@gmail.com>
2026-06-15 20:17:27 +0530

    changed it to accomodate PostgreSQL

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ DATABASE_URL = settings.DATABASE_URL
 
 engine = create_engine(
     DATABASE_URL,
-    connect_args={"check_same_thread":False}
+   # connect_args={"check_same_thread":False}                   We dont use this in PostgresSQL, only in SQLite
 )
 
 SessionLocal = sessionmaker(
