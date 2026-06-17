c65846ce5600cbf409b0387bef16cdd529b6a287
vansh-gautam-bit <vansh.pku@gmail.com>
2026-06-17 18:36:26 +0530

    created security code for password and access token

--- utils/security.py
+++ utils/security.py
@@ -1,4 +1,8 @@
 from passlib.context import CryptContext
+from jose import jwt
+from datetime import datetime, timedelta
+from config import settings
+
 
 pwd_context = CryptContext(
     schemes=["bcrypt"],
@@ -16,3 +20,20 @@ def verify_password(
         plain_password,
         hashed_password
     )
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+
+    expire = datetime.utcnow() + timedelta(
+        minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
+    )
+
+    to_encode.update({"exp": expire})
+
+    encoded_jwt = jwt.encode(
+        to_encode,
+        settings.JWT_SECRET_KEY,
+        algorithm=settings.JWT_ALGORITHM
+    )
+
+    return encoded_jwt
