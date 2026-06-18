511db87c92b1845d59703df6ea07a9f104124e81
vansh-gautam-bit <vansh.pku@gmail.com>
2026-06-18 13:48:37 +0530

    Add authentication of the user

--- routers/users.py
+++ routers/users.py
@@ -6,6 +6,15 @@ from schemas.users import UserCreate , UserResponse ,UserLogin
 from fastapi import HTTPException
 from utils.security import hash_password
 from utils.security import verify_password , create_access_token
+from jose import JWTError, jwt
+from config import settings
+
+
+from fastapi.security import OAuth2PasswordBearer , OAuth2PasswordRequestForm
+
+oauth2_scheme = OAuth2PasswordBearer(
+    tokenUrl="/auth/login"
+)
 
 router = APIRouter(
     prefix="/auth",
@@ -18,6 +27,46 @@ def get_db():
         yield db
     finally:
         db.close()
+
+def get_current_user(
+        token: str = Depends(oauth2_scheme),
+        db: Session = Depends(get_db)
+):
+        try:
+            payload = jwt.decode(
+            token,
+            settings.JWT_SECRET_KEY,
+            algorithms=[settings.JWT_ALGORITHM]
+            )
+            user_id = payload.get("user_id")
+
+            if user_id is None:
+                raise HTTPException(
+                    status_code=401,
+                    detail="Invalid token"
+                    )
+            
+            user = db.query(User).filter(User.id == user_id).first()
+
+            if not user:
+                raise HTTPException(
+                    status_code=401,
+                    detail="User not found"
+                )        
+            return user
+
+        except JWTError:
+            raise HTTPException(
+                status_code=401,
+                detail="Invalid token"
+    )    
+
+@router.get("/user-context")
+def get_user_context(
+    current_user: User = Depends(get_current_user)
+):
+    return current_user
+
 
 @router.post("/register", response_model=UserResponse)
 def create_user(
@@ -38,10 +87,10 @@ def create_user(
 
 @router.post("/login")
 def login_user(
-    user_data:UserLogin,
+    form_data: OAuth2PasswordRequestForm = Depends(),
     db:Session = Depends(get_db)
 ):
-    user = db.query(User).filter(User.email == user_data.email).first()
+    user = db.query(User).filter(User.email == form_data.username).first()
 
     if not user:
         raise HTTPException(
@@ -50,7 +99,7 @@ def login_user(
         )
     
     if not verify_password(
-        user_data.password,
+        form_data.password,
         user.password_hash
     ):
         raise HTTPException(
@@ -59,7 +108,11 @@ def login_user(
         )
         
     access_token = create_access_token(
-        data={"sub": str(user.id)}
+        data={
+            "user_id": user.id,
+            "email":user.email,
+            "role":user.role.value
+            }
     )    
     
     return{
