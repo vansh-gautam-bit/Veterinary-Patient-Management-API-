f8c5bd948b639bcdbfae020d77ef2ad2f4eb5f64
vansh-gautam-bit <vansh.pku@gmail.com>
2026-06-19 16:14:32 +0530

    added logging and global exception handling

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from fastapi import FastAPI , HTTPException , Request
 from fastapi.responses import JSONResponse
+from fastapi.exceptions import RequestValidationError 
+
 from database import Base , engine
+
 from routers.pets import router as pet_router
 from routers.visits import router as visit_router
 from routers.owners import router as owner_router
 from routers.users import router as user_router
+
 import time
+
+from utils.logger import logger
+
 
 Base.metadata.create_all(bind=engine)
 
@@ -37,6 +44,36 @@ async def http_exception_handler(
         }
     )
 
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(
+    request: Request,
+    exc: RequestValidationError
+):
+    return JSONResponse(
+        status_code=422,
+        content={
+            "success": False,
+            "message": "Validation error"
+        }
+    )
+
+@app.exception_handler(Exception)
+async def general_exception_handler(
+    request: Request,
+    exc: Exception
+):
+    logger.error(
+        f"Unexpected error: {str(exc)}"
+    )
+
+    return JSONResponse(
+        status_code=500,
+        content={
+            "success":False,
+            "message":"Internal server error"
+        }
+    )
+
 @app.middleware("http")
 async def log_requests(request, call_next):
 
@@ -44,11 +81,21 @@ async def log_requests(request, call_next):
     response = await call_next(request)
     response_time = time.time() - start_time
 
-    print(f"Request: {request.method} |"
-          f"{request.url.path} |"
-          f"{response.status_code} |"
-          f"{response_time:.4f} sec|"
+    logger.info(
+        f"Request: {request.method} |"
+        f"{request.url.path} |"
+         f"{response.status_code} |"
+        f"{response_time:.4f} sec|"
 
-          )
+    )
     
     return response
+
+@app.on_event("startup")
+async def startup_event():
+    logger.info("Application started")
+
+@app.on_event("shutdown")
+async def startup_event():
+    logger.info("Application stopped")    
+
