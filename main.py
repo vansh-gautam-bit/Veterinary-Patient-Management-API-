5fbd988eb7a612762fc5c2c23660624f9d69dbc3
vansh-gautam-bit <vansh.pku@gmail.com>
2026-07-29 18:14:00 +0530

    Add live website in CORS middleware

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
+        "https://vet-pals-clinic.lovable.app",
         "https://id-preview--ae1d4e9b-122a-4bd8-9264-b23ac7416bfb.lovable.app",
         "https://id-preview-ae1d4e9b-122a-4bd8-9264-b23ac7416bfb.lovable.app",
     ],
